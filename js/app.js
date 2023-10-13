3a4f0e4a920cb036b6ec94d9d4cd4cf510c214ef
FedericaSidoti <federicasidoti21@gmail.com>
2023-10-13 17:11:07 +0200

    ADD- bonus

--- js/app.js
+++ js/app.js
@@ -8,22 +8,35 @@ btnPlayDomElement.addEventListener('click', function() {
 //    - svuotare eventualmente la griglia
     flexGridContainerDomElement.innerHTML = ''
 
-    for(let i = 0; i < 100; i++){
+    const selectDomElement = document.getElementById('levels')
+    const selectValue = selectDomElement.options[selectDomElement.selectedIndex].value
+    const numberCells = parseInt(selectValue)
+    
+
+    for(let i = 0; i < numberCells; i++){
         let n = i + 1; 
         const cellDomElement = `
         <div class="flex-grid-item">${n}</div>
         `
         flexGridContainerDomElement.innerHTML += cellDomElement 
+        
     }
 //    - aggiungere l' eventlistener alla casella stampata con parametri click, ONCLICKCELL
     const cellDomElementNodes = document.querySelectorAll('.flex-grid-item') 
     
 
-    for( let i = 0; i < cellDomElementNodes.length; i++){
+    for(let i = 0; i < cellDomElementNodes.length; i++){
         let currentCell = cellDomElementNodes[i];
         //console.log(currentCell)
 
         currentCell.addEventListener('click', onClickCell)
+        if (selectValue == 81) {
+            currentCell.classList.add('medium-level')
+        } else if (selectValue == 49) {
+            currentCell.classList.add('hard-level')
+        } else {
+            currentCell.classList.add('easy-level')
+        }
     }  
 
 })
